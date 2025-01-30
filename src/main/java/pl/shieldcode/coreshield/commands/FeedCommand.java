b7eff62722a0cb3fae3ae5b3d95807f1799eebfe
SzymonZar <151452235+SzymonZar@users.noreply.github.com>
2025-01-30 13:19:23 +0100

    Update FeedCommand.java

--- src/main/java/pl/shieldcode/coreshield/commands/FeedCommand.java
+++ src/main/java/pl/shieldcode/coreshield/commands/FeedCommand.java
@@ -1,8 +1,7 @@
-package pl.shieldcode.coreshield;
+package pl.shieldcode.coreshield.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
