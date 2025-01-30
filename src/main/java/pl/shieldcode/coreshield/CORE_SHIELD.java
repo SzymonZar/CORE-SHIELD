82d2de49561b900ef1a0a5e12f51d60ab3f2ed02
SzymonZar <151452235+SzymonZar@users.noreply.github.com>
2025-01-30 13:21:19 +0100

    Update CORE_SHIELD.java

--- src/main/java/pl/shieldcode/coreshield/CORE_SHIELD.java
+++ src/main/java/pl/shieldcode/coreshield/CORE_SHIELD.java
@@ -1,17 +1,25 @@
 package pl.shieldcode.coreshield;
 
+
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.shieldcode.coreshield.commands.*;
 
 public final class CORE_SHIELD extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Plugin CORE-SHIELD włącza się...");
+
+        getCommand("gm").setExecutor(new GmCommand());
+        getCommand("heal").setExecutor(new HealCommand());
+        getCommand("feed").setExecutor(new FeedCommand());
+        getCommand("repair").setExecutor(new RepairCommand());
+        getCommand("pomoc").setExecutor(new HelpCommand()); // Nieskonczone
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().severe("Plugin CORE-SHIELD wyłącza się...");
     }
 }
